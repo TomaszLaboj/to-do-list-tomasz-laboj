f6464f932615b04d6d8997785f0856b9baa94cc9
TomaszLaboj <tomasz.laboj@gmail.com>
2023-08-05 18:19:47 +0100

    formatted

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,24 @@ import todolist from "./components/toDoList.json";
 import { useState } from "react";
 import "./App.css";
 
-function App():JSX.Element {
+function App(): JSX.Element {
   const today = new Date().toISOString().substring(0, 10);
   const initialListOfTasks: oneTask[] = todolist;
 
   const [listOfTasks, setListOfTasks] = useState<oneTask[]>(initialListOfTasks);
   const [addDescription, setAddDescription] = useState<string>("");
   const [addDate, setAddDate] = useState<string>(today);
-  const lastIndexOfInitialListOfTasks: number = initialListOfTasks.length+1;
+  const lastIndexOfInitialListOfTasks: number = initialListOfTasks.length + 1;
   const handleAddTask = () => {
-    console.log(addDescription,addDate)
+    console.log(addDescription, addDate);
     initialListOfTasks.push({
       description: addDescription,
       dateAdded: today,
       dueDate: addDate,
       status: "In progress",
-      id: lastIndexOfInitialListOfTasks
+      id: lastIndexOfInitialListOfTasks,
     });
-    
+
     setListOfTasks([...initialListOfTasks]);
     setAddDescription("");
     setAddDate(today);
@@ -28,29 +28,27 @@ function App():JSX.Element {
   return (
     <>
       <h1>Create new task</h1>
-     
-        <p>Description: </p>{" "}
-        <input
-          type="text"
-          id="description"
-          name="description"
-          onChange={(event) => {
-            setAddDescription(event.target.value);
-          }}
-        />
-        <br />
-        <p>Due Date: </p>
-        <input
-          type="date"
-          value={addDate}
-          placeholder="dd/mm/yyyy"
-          onChange={(event) => {
-            setAddDate(event.target.value);
-          }}
-        />
-        <br />
-     
-        <button onClick={handleAddTask}>Add task</button>
+      <p>Description: </p>{" "}
+      <input
+        type="text"
+        id="description"
+        name="description"
+        onChange={(event) => {
+          setAddDescription(event.target.value);
+        }}
+      />
+      <br />
+      <p>Due Date: </p>
+      <input
+        type="date"
+        value={addDate}
+        placeholder="dd/mm/yyyy"
+        onChange={(event) => {
+          setAddDate(event.target.value);
+        }}
+      />
+      <br />
+      <button onClick={handleAddTask}>Add task</button>
       <h1>To do list</h1>
       <h1>{addDescription}</h1>
       <h1>{addDate}</h1>
